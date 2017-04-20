38c5075af8fb1974957c9c0b76d03f00c884a422
eratzlaff <eratzlaff@adelantos.com.py>
2017-04-19 22:38:48 -0400

    fix run.sh name

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker rm wildflykeycloak-adapter
-docker build --no-cache --tag=adelantos/wildflykeycloak-adapter .
-docker run -it -p 8080:8080 --name=wildflykeycloak-adapter --network=host \
+docker rm wildflykeycloak-pg-adapter
+docker build --no-cache --tag=adelantos/wildflykeycloak-pg-adapter .
+docker run -it -p 8080:8080 --name=wildflykeycloak-pg-adapter --network=host \
     -e POSTGRES_PORT_5432_TCP_ADDR=localhost -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=postgres adelantos/wildflykeycloak-adapter
+    -e POSTGRES_PASSWORD=postgres adelantos/wildflykeycloak-pg-adapter
